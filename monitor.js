975cbb7b90a508513802fe1b2331af1fc60fed25
Daniel Moore <63226572+wron1@users.noreply.github.com>
2022-02-09 23:10:17 -0800

    Adding sneaker test to test monitor

--- monitor.js
+++ monitor.js
@@ -1,1 +1,9 @@
 console.log("This is a test")
+
+const thisProject = this.project
+
+function testing(){
+  if(thisProject === "Getting Scraped"){
+    console.log("Wow testing, wow getting scraped, wow sneaker bots are scraping this project because it has Sneaker in the title!")
+  } 
+}
